355083ec19f077762ad0bff4cef7e5e630288a22
kimboslice99 <94807745+kimboslice99@users.noreply.github.com>
2025-02-28 00:11:56 -0500

    rename

--- funcs.go
+++ funcs.go
@@ -68,8 +68,8 @@ func forceRoom(c *Client, message string) {
 	c.send <- forceRoomJson
 }
 
-// etagMiddleware adds ETag headers to static file responses and handles conditional requests.
-func etagMiddleware(next http.Handler) http.Handler {
+// middleware adds ETag headers to static file responses and handles conditional requests.
+func middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		filePath := "html" + r.URL.Path
 		if stat, err := os.Stat(filePath); err == nil && !stat.IsDir() {
